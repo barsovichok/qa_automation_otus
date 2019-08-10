c5262db891d0f17797c02356953b1fc20898c964
barsovichok <taya.kulagina@gmail.com>
2019-08-10 22:37:17 +0500

    add docstrings

--- Pytest_introduction/python_types.py
+++ Pytest_introduction/python_types.py
@@ -1,28 +1,29 @@
-def sum_integers(a, b):
+def sum_integers(int1, int2):
     # type: (integer, integer) -> integer
-    return a+b
+    return int1 + int2
 
-def lists_count(a,b):
+
+def lists_count(list1, list2):
     # type: (list, list) -> list
-    return a.count(5) + b.count("f")
+    return list1.count(5) + list2.count("f")
 
 
-def lists_append(a):
+def lists_append(list):
     # type: (list) -> bool
-    c = len(a)
-    a.append('Лапка')
-    b = len(a)
-    return c < b
+    len_list = len(list)
+    list.append('Лапка')
+    len_new_list = len(list)
+    return len_list < len_new_list
 
 
-def strings_concat(a,b,c):
+def strings_concat(string1, string2, string3):
     # type: (string, string, string) -> string
-    return f"{a}{b}{c}"
+    return f"{string1}{string2}{string3}"
 
 
-def strings_len(a: object) -> object:
+def strings_len(strings_len):
     # type: (string) -> int
-    return len(a)
+    return len(strings_len)
 
 
 def dictionaries_get(product, key_product):
@@ -42,27 +43,6 @@ def sets(set1, set2):
     return set1 in set2
 
 
-def add_set(b):
-    b.add(1)
-    return b
-
-#
-# strings_concat = strings_concat(a="Кот ", b="по имени ", c="Питон")
-# sum_integers = sum_integers(4, 5)
-# lists_append = lists_append(a=[4, 5, 'i'])
-# lists_count = lists_count(a=[4, 5, 'i'], b=[10, "f", "f", "9"])
-# strings_len = strings_len("мармозетка")
-# dictionaries_get= dictionaries_get(product={"name": "iPhone Xs","stock": 24,"price": 65432.1}, key_product="stock")
-# list_dicts = list_dicts(stock=[{'name': 'iPhone Xs Plus',
-#                             'stock': 24,
-#                             'price': 65432.1,
-#                             'recommended': ['Samsung Galaxy S10', 'iPhone Xs']},
-#                            {'name': 'Samsung Galaxy S10', 'stock': 8, 'price': 50000.0, 'discount': 5000},
-#                            {'name': 'Xiaomi Mi8',
-#                             'stock': 42,
-#                             'price': 38000.5}], number=0)
-# tuples = tuples(tuple=(1, 3, 4, "fish", 4))
-# sets = sets(set1={'son', 'daddy', 'mum'}, set2={'sugar', 'lemon', 'ice'})
-# add_set = add_set(b={'sugar', 'lemon', 'ice'})
-
-
+def add_set(add_set):
+    add_set.add(1)
+    return add_set
--- Pytest_introduction/python_types_test.py
+++ Pytest_introduction/python_types_test.py
@@ -6,24 +6,61 @@ from python_types import dictionaries_get, sets, add_set, sum_integers, lists_co
 class TestPythonExamples:
 
     def test_integers(self, sum_integers_data):
+        """
+        Test checks Python data type "integer"
+        :param sum_integers_data:
+        :return: sum of two integers
+        """
+
         assert sum_integers(*sum_integers_data) == 9
 
     def test_lists_count(self, list_count_data):
+        """
+        Test checks Python data type "list"
+        :param list_count_data:
+        :return: count some data in list
+        """
         assert lists_count(*list_count_data) == 3
 
     def test_lists_append(self, lists_append_data):
+
+        """
+        Test checks Python data type "list" (add new data in list)
+        :param lists_append_data:
+        :return: new data in list
+        """
         assert lists_append(lists_append_data) is True
 
     def test_strings_concat(self, strings_concat_data):
+        """
+        Test checks Python data type "string"
+        :param strings_concat_data:
+        :return: concatenate string from seeral strings
+        """
         assert strings_concat(*strings_concat_data) == "Кот по имени Питон"
 
     def test_strings_len(self, strings_len_data):
+        """
+        Test checks Python data type "string"
+        :param strings_len_data:
+        :return: integer - string lenght
+        """
         assert strings_len(strings_len_data) == 10
 
     def test_dictionaries_get(self, dictionaries_get_data):
+        """
+        Test checks Python data type "dictionary"
+        :param dictionaries_get_data:
+        :return: value by key
+        """
         assert dictionaries_get(*dictionaries_get_data) == 24
 
     def test_list_dicts(self, list_dicts_data):
+        """
+        Test checks Python data type "dictionary"
+        :param list_dicts_data:
+        :return: value from  by meaning
+        """
         assert list_dicts(*list_dicts_data) == {'name': 'iPhone Xs Plus',
                          'stock': 24,
                          'price': 65432.1,
@@ -32,10 +69,26 @@ class TestPythonExamples:
                               'iPhone Xs']}
 
     def test_tuples(self, tuples_data):
+        """
+        Test checks Python data type "tuple"
+        :param tuples_data:
+        :return: concatenate string data from tple
+        """
         assert tuples(tuples_data) == "34fish"
 
     def test_sets(self, sets_data):
+        """
+        Test checks Python type "set"
+        :param sets_data:
+        :return: boolean False - wrong try to find some common data in two sets
+        """
         assert sets(*sets_data) == False
 
     def test_add_set(self, add_set_data):
+        """
+        Test checks free addition some data to type set
+
+        :param add_set_data:
+        :return: add data to set
+        """
         assert add_set(add_set_data) == {'sugar', 'lemon', 'ice', 1}

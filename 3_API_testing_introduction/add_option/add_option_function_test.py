6e4245d3bc45d03721bb605c50ad9912dccf737e
barsovichok <taya.kulagina@gmail.com>
2019-08-17 22:59:29 +0500

    add docstring

--- 3_API_testing_introduction/add_option/add_option_function_test.py
+++ 3_API_testing_introduction/add_option/add_option_function_test.py
@@ -2,6 +2,11 @@ import requests
 
 
 def test_status_code(url_param):
+    """
+    Проверяем входящий параметр (урл) на код статуса - должен быть 200
+    :param url_param:
+    :return:
+    """
     status_code = requests.get(url_param).status_code
     assert status_code == 200
 

8aedd59eae87f149f127bbbba707cf5266ff0805
barsovichok <taya.kulagina@gmail.com>
2019-08-17 22:58:37 +0500

    delete prints

--- 3_API_testing_introduction/add_option/add_option_function_test.py
+++ 3_API_testing_introduction/add_option/add_option_function_test.py
@@ -3,8 +3,6 @@ import requests
 
 def test_status_code(url_param):
     status_code = requests.get(url_param).status_code
-    print(url_param)
-    print(status_code)
     assert status_code == 200
 
 

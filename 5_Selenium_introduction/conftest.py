13b6c11976c9d012d8165119a45aa4fb79e46ea7
barsovichok <taya.kulagina@gmail.com>
2019-08-25 21:13:03 +0500

    add selenium intro + opencart

--- 5_Selenium_introduction/conftest.py
+++ 5_Selenium_introduction/conftest.py
@@ -8,27 +8,48 @@ def pytest_addoption(parser):
         action="store",
         default="ie",
         help="This is request browser",
-        required=True
+        required=False
     )
 
-@pytest.fixture
+    parser.addoption(
+        "--opencart_url",
+        action="store",
+        default="http://localhost/OpencartStore/",
+        help="This is opencart_url",
+        required=False
+    )
+
+
+
+@pytest.fixture()
 def browser_param(request):
     return request.config.getoption("--browser")
 
 
 @pytest.fixture()
+def opencart_url_param(request):
+    return request.config.getoption("--opencart_url")
+
+
+@pytest.fixture()
 def start_driver(browser_param):
     if browser_param == "chrome":
-        driver = webdriver.Chrome("C:\Tools\chromedriver.exe")
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(options=options)
         return driver
-        # driver.get("http://www.python.org")
-        # assert "Python" in driver.title
-        # driver.close()
     elif browser_param == "firefox":
-        driver = webdriver.Chrome("C:\Tools\geckodriver.exe")
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
         return driver
-        # driver.get("http://www.python.org")
-        # assert "Python" in driver.title
-        # driver.close()
-    #request.addFinalizer(wd.quit)
-    #return wd
+    elif browser_param == "ie":
+        options = webdriver.IeOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Ie(options=options)
+        return driver
+
+
+@pytest.fixture()
+def opencart_url(opencart_url_param):
+    return opencart_url_param
--- 5_Selenium_introduction/test_selenium_intro.py
+++ 5_Selenium_introduction/test_selenium_intro.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 import pytest
 
 
-def test_stаrt_driver(start_driver):
+def test_stаrt_driver(start_driver, opencart_url):
     """
     Проверяем, что в каждом браузере открывается страница
     :param url_param:
     :return:
     """
-    start_driver.get("http://www.python.org")
-    assert start_driver.title == "Welcome to Python.org"
+    start_driver.get(opencart_url)
+    assert start_driver.title == 'Your Store'
+    start_driver.close()
 
